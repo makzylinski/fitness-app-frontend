fe3bba022e5ebd1caf1a7b224ae2ad7d34e593e7
Maks <makzylinski@gmail.com>
2025-07-29 19:24:59 +0200

    Add toolbar component with navigation links and integrate into app layout

---
+++ src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ToolbarComponent {
+
+}
