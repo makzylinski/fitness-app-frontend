eeaec195f7e11abeac03415523d2df0d1023269a
Maks <makzylinski@gmail.com>
2025-07-30 21:54:31 +0200

    Add dashboard card component with HTML, SCSS, and TypeScript files

---
+++ src/app/components/dashboard/dashboard-card/dashboard-card.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-card.component.html',
+  styleUrl: './dashboard-card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DashboardCardComponent {
+
+}
