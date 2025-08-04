f797e8bfa43b3c598bc84f0e16e59189eede2668
Maks <makzylinski@gmail.com>
2025-08-04 20:29:52 +0200

    Add workouts component with form for logging strength training sessions

--- src/app/components/workouts/workouts.component.ts
+++ src/app/components/workouts/workouts.component.ts
@@ -1,13 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-workouts',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './workouts.component.html',
   styleUrl: './workouts.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WorkoutsComponent {
+  workoutForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.workoutForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit(): void {
+    console.log('Submit');
+  }
+
+  get name() {
+    return this.workoutForm.get('name');
+  }
 }
