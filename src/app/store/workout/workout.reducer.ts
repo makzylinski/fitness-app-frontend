7926c4012562e3010813858f7c784157977a5c3d
Maks <makzylinski@gmail.com>
2025-09-03 18:20:18 +0200

    Refactor workout actions and reducer for improved type safety and state management

--- src/app/store/workout/workout.reducer.ts
+++ src/app/store/workout/workout.reducer.ts
@@ -1,8 +1,12 @@
 import { createReducer, on } from '@ngrx/store';
-import { setExercise } from './workout.actions';
+import { reset, setExercise } from './workout.actions';
 import { initialWorkoutState } from './workout.model';
 
 export const workoutReducer = createReducer(
   initialWorkoutState,
-  on(setExercise, (state, { exercise }) => ({ ...state, exercise }))
+  on(setExercise, (state, { exercise }) => ({
+    ...state,
+    exercise: [...state.exercise, ...exercise],
+  })),
+  on(reset, () => initialWorkoutState)
 );
