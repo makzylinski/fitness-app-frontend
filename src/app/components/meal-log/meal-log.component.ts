6dc063c9b987c3e27b6520f599003a2a1d11db82
Maks <makzylinski@gmail.com>
2025-11-18 10:51:56 +0100

    Add InputComponent to MealLogComponent and update template

--- src/app/components/meal-log/meal-log.component.ts
+++ src/app/components/meal-log/meal-log.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { InputComponent } from '../../shared/components/input/input.component';
 
 @Component({
   selector: 'app-meal-log',
   standalone: true,
-  imports: [],
+  imports: [InputComponent],
   templateUrl: './meal-log.component.html',
   styleUrl: './meal-log.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
