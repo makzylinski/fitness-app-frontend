d8957a633808f59df266231ec97be26b73b7b8a8
Maks <makzylinski@gmail.com>
2025-11-18 11:55:13 +0100

    Refactor MealLogComponent to integrate ReactiveFormsModule and structure form inputs for better form handling

--- src/app/components/meal-log/meal-log.component.ts
+++ src/app/components/meal-log/meal-log.component.ts
@@ -1,14 +1,25 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { InputComponent } from '../../shared/components/input/input.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-meal-log',
   standalone: true,
-  imports: [InputComponent],
+  imports: [ReactiveFormsModule, InputComponent],
   templateUrl: './meal-log.component.html',
   styleUrl: './meal-log.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MealLogComponent {
+  mealLogForm: FormGroup;
+
+  constructor(
+      private formBuilder: FormBuilder
+    ) {
+      this.mealLogForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+      });
+    }
 
 }
