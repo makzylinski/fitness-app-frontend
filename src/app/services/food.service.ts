753c13feb38ed3e2ae862b864e30d98866604867
Maks <makzylinski@gmail.com>
2025-11-19 10:55:28 +0100

    Refactor FoodService to update API endpoint and implement getProductByBarcode method

--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -1,4 +1,4 @@
-//https://world.openfoodfacts.net/api/v2/product/3274080005003.json
+//https://world.openfoodfacts.net/api/v2/product/{barcode}
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -6,8 +6,12 @@ import { Injectable } from '@angular/core';
     providedIn: 'root',
 })
 export class FoodService {
-    private readonly foodUrl = `https://world.openfoodfacts.net/api/v2/`;
+    private readonly foodUrl = `https://world.openfoodfacts.net/api/v2/product/`;
 
     constructor(private http: HttpClient) { }
 
+    getProductByBarcode = (barcode: string) =>{
+        const url = `${this.foodUrl}${barcode}.json`;
+        return this.http.get(url);
+    }
 }
