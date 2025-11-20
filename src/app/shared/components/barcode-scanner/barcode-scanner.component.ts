822ab161940f7b725f1b0e11fa0c136594d7273e
Maks <makzylinski@gmail.com>
2025-11-20 09:38:48 +0100

    feat: add ZXing barcode scanner integration and update component structure

--- src/app/shared/components/barcode-scanner/barcode-scanner.component.ts
+++ src/app/shared/components/barcode-scanner/barcode-scanner.component.ts
@@ -1,26 +1,36 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-barcode-scanner',
   standalone: true,
-  imports: [],
+  imports: [ZXingScannerModule],
   templateUrl: './barcode-scanner.component.html',
   styleUrl: './barcode-scanner.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BarcodeScannerComponent implements AfterViewInit {
-  @ViewChild('videoElement') videoElement!: any;
-
-  ngAfterViewInit(): void {
-    const video = this.videoElement.nativeElement as HTMLVideoElement;
-    if (navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({ video: true })
-        .then(function (stream) {
-          video.srcObject = stream;
-        })
-        .catch(function (err) {
-          console.log("Something went wrong!");
-        });
-    }
+export class BarcodeScannerComponent {
+  scanCompleteHandler(result: any): void {
+    console.log('Scan Complete:', result);
   }
+
+  scanSuccessHandler(result: any): void {
+    console.log('Scan Success:', result);
+  }
+
+
+  // @ViewChild('videoElement') videoElement!: any;
+
+  // ngAfterViewInit(): void {
+  //   const video = this.videoElement.nativeElement as HTMLVideoElement;
+  //   if (navigator.mediaDevices.getUserMedia) {
+  //     navigator.mediaDevices.getUserMedia({ video: true })
+  //       .then(function (stream) {
+  //         video.srcObject = stream;
+  //       })
+  //       .catch(function (err) {
+  //         console.log("Something went wrong!");
+  //       });
+  //   }
+  // }
 }
