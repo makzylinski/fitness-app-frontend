bba73f02485853ea836b530da968821603075b7d
Maks <makzylinski@gmail.com>
2025-11-25 13:37:49 +0100

    feat: add product search functionality in FoodService and log results in MealLogComponent

--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -7,11 +7,24 @@ import { Injectable } from '@angular/core';
 })
 export class FoodService {
     private readonly foodUrl = `https://world.openfoodfacts.net/api/v2/product/`;
+    private readonly foodSearchUrl = `https://world.openfoodfacts.net/cgi/search.pl?search_terms=`;
+    //https://world.openfoodfacts.org/cgi/search.pl?search_terms=coke&search_simple=1&jqm=1
+
 
     constructor(private http: HttpClient) { }
 
     getProductByBarcode = (barcode: string) =>{
         const url = `${this.foodUrl}${barcode}.json`;
         return this.http.get(url, { headers: { Authorization: "Basic " + btoa("off:off") } });
+    }
+
+    searchProducts = (query: string) => {
+        const url = `https://world.openfoodfacts.org/api/v2/search?categories_tags_en=chocolate&fields=product_name,brands,nutriscore_grade`;
+        return this.http.get(url, { headers: { Authorization: "Basic " + btoa("off:off") } });
+    }
+
+    searchProduct2 = (query: string) => {
+        const url = `https://world.openfoodfacts.org/api/v2/search?query=nutella`;
+        return this.http.get(url, { headers: { Authorization: "Basic " + btoa("off:off") } });  
     }
 }
