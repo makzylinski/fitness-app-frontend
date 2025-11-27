1d4c2b3ba4fc676e3dbe5916af39447d992f819a
Maks <makzylinski@gmail.com>
2025-11-27 12:27:06 +0100

    refactor: remove outdated comments and clean up FoodService code

--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -1,4 +1,3 @@
-//https://world.openfoodfacts.net/api/v2/product/{barcode}
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -8,8 +7,6 @@ import { Injectable } from '@angular/core';
 export class FoodService {
     private readonly foodUrl = `https://world.openfoodfacts.net/api/v2/product/`;
     private readonly foodSearchUrl = `https://world.openfoodfacts.net/cgi/search.pl?search_terms=`;
-    //https://world.openfoodfacts.org/cgi/search.pl?search_terms=coke&search_simple=1&jqm=1
-
 
     constructor(private http: HttpClient) { }
 
@@ -17,6 +14,8 @@ export class FoodService {
         const url = `${this.foodUrl}${barcode}.json`;
         return this.http.get(url, { headers: { Authorization: "Basic " + btoa("off:off") } });
     }
+
+    //TODO: This API is not working as expected, fix it later
 
     searchProducts = (query: string) => {
         const url = `https://world.openfoodfacts.org/api/v2/search?categories_tags_en=chocolate&fields=product_name,brands,nutriscore_grade`;
