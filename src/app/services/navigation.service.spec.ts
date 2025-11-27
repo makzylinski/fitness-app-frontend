6b27e273da477c7da2a9367ae955af77910a3ffe
Maks <makzylinski@gmail.com>
2025-11-27 15:22:04 +0100

    test: update NavigationService tests to include login navigation

--- src/app/services/navigation.service.spec.ts
+++ src/app/services/navigation.service.spec.ts
@@ -1,4 +1,4 @@
-import { Router } from "@angular/router";
+import { Route, Router } from "@angular/router";
 import { NavigationService } from "./navigation.service";
 import { TestBed } from "@angular/core/testing";
 
@@ -21,5 +21,17 @@ describe('NavigationService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should navigate to login', async () => {
+        const routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate']);
+        routerSpy.navigate.and.returnValue(Promise.resolve(true) as any);
+
+        const service = new NavigationService(routerSpy as unknown as Router);
+
+        const result = service.navigateToLogin();
+
+        expect(routerSpy.navigate).toHaveBeenCalledOnceWith(['/login']);
+        await expectAsync(result).toBeResolvedTo(true);
     });
 });
