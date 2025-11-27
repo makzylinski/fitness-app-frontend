5b9d03cc8e55f16566b200a441b5d8ae0464d021
Maks <makzylinski@gmail.com>
2025-11-27 14:48:20 +0100

    test: add unit tests for NavigationService

--- src/app/services/navigation.service.spec.ts
+++ src/app/services/navigation.service.spec.ts
@@ -1,3 +1,25 @@
-describe('NavigationService', () => {
+import { Router } from "@angular/router";
+import { NavigationService } from "./navigation.service";
+import { TestBed } from "@angular/core/testing";
 
+describe('NavigationService', () => {
+    let service: NavigationService;
+    const routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate', 'navigateByUrl']);
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                NavigationService,
+                {
+                    provide: Router,
+                    useValue: routerSpy
+                }
+            ],
+        });
+        service = TestBed.inject(NavigationService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
